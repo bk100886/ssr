e4671711a8ff94ea3f8204bf2bebac2b77fa12ff
bk100886 <62583920+bk100886@users.noreply.github.com>
2022-05-29 19:08:49 +0300

    Update index.tsx

--- src/pages/inscompanies/index.tsx
+++ src/pages/inscompanies/index.tsx
@@ -2,11 +2,17 @@
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
+import { styled, useTheme } from '@mui/material/styles'
 
 // ** Demo Components Imports
-import TableBasic from 'src/views/tables/TableBasic'
 import { CardContent } from '@mui/material'
 import Link from 'next/link'
+
+const LinkStyled = styled('a')(({ theme }) => ({
+  fontSize: '0.875rem',
+  textDecoration: 'none',
+  color: theme.palette.primary.main
+}))
 
 const InsCompanies = () => {
   return (
@@ -17,9 +23,13 @@ const InsCompanies = () => {
           {/* <TableBasic /> */}
           <CardContent>
             
-            <Link passHref href="/inscompanies/edit/1">Перейти на конкретную компанию</Link>
+            <Link passHref href='/inscompanies/edit/1'>
+              <LinkStyled>Перейти на конкретную компанию</LinkStyled>
+            </Link>
             <br/>
-            <Link passHref href="/inscompanies/create">создать</Link>
+            <Link passHref href='/inscompanies/create'>
+              <LinkStyled>Создать</LinkStyled>
+            </Link>
           </CardContent>
         </Card>
       </Grid>
